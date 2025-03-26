0ef401e0605d112c077d2182cbe3acb97262c958
tosbas <tosbasdev@protonmail.com>
2025-03-26 20:10:57 +0100

    Chargement d'un model exemple

--- controllers/Main.php
+++ controllers/Main.php
@@ -4,9 +4,16 @@ use App\Controller;
 
 class Main extends Controller
 {
-    
+    /**
+     * @var MyModel
+     */
+    protected $MyModel;
+
     public function index()
     {
-        $this->render("Home", "Ma description");
+        $this->loadModel("MyModel");
+        $my_datas = $this->MyModel->getAll();
+
+        $this->render("Home", "Ma description", $my_datas);
     }
 }
