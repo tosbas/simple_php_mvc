de67071d571a321ccb8fa9fdcf1895d80c646f2d
tosbas <tosbasdev@protonmail.com>
2025-03-28 16:18:14 +0100

    Mise à jour du layout pour inclure les fichiers CSS et JS dynamiques dans le template.

--- views/layout/default.php
+++ views/layout/default.php
@@ -4,14 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?= $this->description ?>" />
-    <title><?= $this->title ?></title>
+    <meta name="description" content="<?= $pageDesc ?>" />
+    <?= $pageCssFiles ?>
+    <title><?= $pageTitle ?></title>
 </head>
 
 <body>
 
     <?= $content ?>
 
+    <?= $pageScripts ?>
 </body>
 
 </html>
